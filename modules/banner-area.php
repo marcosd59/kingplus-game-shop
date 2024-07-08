bf98cba7e1be3baeaf5b705ebc255db8ef58a821
marcosd59 <damian.marcospool@gmail.com>
2024-07-08 07:14:21 -0500

    refactor: Update banner area to include new images

---
+++ modules/banner-area.php
@@ -0,0 +1,31 @@
+<div class="ltn__banner-area mt-120">
+	<div class="container">
+		<div class="row ltn__custom-gutter--- justify-content-center">
+			<div class="col-lg-6 col-md-6">
+				<div class="ltn__banner-item">
+					<div class="ltn__banner-img">
+						<a href="shop.php"><img src="<?php echo $ref_rel; ?>img/banner/13.jpg" alt="Banner Image" /></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="ltn__banner-item">
+							<div class="ltn__banner-img">
+								<a href="shop.php"><img src="<?php echo $ref_rel; ?>img/banner/14.jpg" alt="Banner Image" /></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-12">
+						<div class="ltn__banner-item">
+							<div class="ltn__banner-img">
+								<a href="shop.php"><img src="<?php echo $ref_rel; ?>img/banner/15.jpg" alt="Banner Image" /></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
