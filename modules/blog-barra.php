39fede64af28e8dee10083bcbd041b579f13d841
marcosd59 <damian.marcospool@gmail.com>
2024-07-08 07:14:34 -0500

    refactor: Update Vue components for displaying shopping cart and product search

---
+++ modules/blog-barra.php
@@ -0,0 +1,215 @@
+		<!-- Search Widget -->
+		<div class="widget ltn__search-widget">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Buscar Objetos</h4>
+			<form action="#">
+				<input type="text" name="search" placeholder="Palabra clave...">
+				<button type="submit"><i class="fas fa-search"></i></button>
+			</form>
+		</div>
+		<!-- Popular Post Widget -->
+		<div class="widget ltn__popular-post-widget">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Articulos Populares</h4>
+			<ul>
+				<li>
+					<div class="popular-post-widget-item clearfix">
+						<div class="popular-post-widget-img">
+							<a href="#"><img src="<?php echo $ref_rel; ?>img/articulos/1.jpg" alt="#"></a>
+						</div>
+						<div class="popular-post-widget-brief">
+							<h6><a href="#">Novedades en The Legend of Zelda: Breath of the Wild 2</a></h6>
+							<div class="ltn__blog-meta">
+								<ul>
+									<li class="ltn__blog-date">
+										<a href="#"><i class="far fa-calendar-alt"></i>Junio 22, 2024</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="popular-post-widget-item clearfix">
+						<div class="popular-post-widget-img">
+							<a href="#"><img src="<?php echo $ref_rel; ?>img/articulos/2.jpg" alt="#"></a>
+						</div>
+						<div class="popular-post-widget-brief">
+							<h6><a href="#">E3 2024: Resumen de los Anuncios Más Importantes</a></h6>
+							<div class="ltn__blog-meta">
+								<ul>
+									<li class="ltn__blog-date">
+										<a href="#"><i class="far fa-calendar-alt"></i>June 22, 2020</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="popular-post-widget-item clearfix">
+						<div class="popular-post-widget-img">
+							<a href="#"><img src="<?php echo $ref_rel; ?>img/articulos/3.jpg" alt="#"></a>
+						</div>
+						<div class="popular-post-widget-brief">
+							<h6><a href="#">Actualización de Cyberpunk 2077: Mejoras y Nuevo Contenido</a></h6>
+							<div class="ltn__blog-meta">
+								<ul>
+									<li class="ltn__blog-date">
+										<a href="#"><i class="far fa-calendar-alt"></i>June 22, 2020</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="popular-post-widget-item clearfix">
+						<div class="popular-post-widget-img">
+							<a href="#"><img src="<?php echo $ref_rel; ?>img/articulos/4.jpg" alt="#"></a>
+						</div>
+						<div class="popular-post-widget-brief">
+							<h6><a href="#">Lanzamiento de Starfield: Todo lo que Necesitas Saber</a></h6>
+							<div class="ltn__blog-meta">
+								<ul>
+									<li class="ltn__blog-date">
+										<a href="#"><i class="far fa-calendar-alt"></i>June 22, 2020</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<!-- Menu Widget (Category) -->
+		<div class="widget ltn__menu-widget ltn__menu-widget-2 ltn__menu-widget-2-color-2">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Categorías</h4>
+			<ul>
+				<li><a href="#">Noticias <span>26</span></a></li>
+				<li><a href="#">Videojuegos <span>30</span></a></li>
+				<li><a href="#">Tecnologia <span>71</span></a></li>
+				<li><a href="#">Consolas<span>56</span></a></li>
+				<li><a href="#">Nuevos Lanzamientos<span>60</span></a></li>
+			</ul>
+		</div>
+		<!-- Social Media Widget -->
+		<div class="widget ltn__social-media-widget">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Nunca te pierdas las noticias</h4>
+			<div class="ltn__social-media-2">
+				<ul>
+					<li><a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+					<li><a href="#" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+					<li><a href="#" title="Linkedin"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="#" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+					<li><a href="#" title="Behance"><i class="fab fa-behance"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- Popular Post Widget (Twitter Post) -->
+		<div class="widget ltn__popular-post-widget ltn__twitter-post-widget">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Feeds de Twitter</h4>
+			<ul>
+				<li>
+					<div class="popular-post-widget-item clearfix">
+						<div class="popular-post-widget-img">
+							<a href="#"><i class="fab fa-twitter"></i></a>
+						</div>
+						<div class="popular-post-widget-brief">
+							<p>Carsafe - #Gutenberg ready
+								@wordpress
+								Theme for Car Service, Auto Parts, Car Dealer available on
+								@website
+								<a href="<?php echo $ref_rel; ?>https://website.net">https://website.net</a>
+							</p>
+							<div class="ltn__blog-meta">
+								<ul>
+									<li class="ltn__blog-date">
+										<a href="#"><i class="far fa-calendar-alt"></i>June 22, 2020</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="popular-post-widget-item clearfix">
+						<div class="popular-post-widget-img">
+							<a href="#"><i class="fab fa-twitter"></i></a>
+						</div>
+						<div class="popular-post-widget-brief">
+							<p>Carsafe - #Gutenberg ready
+								@wordpress
+								Theme for Car Service, Auto Parts, Car Dealer available on
+								@website
+								<a href="<?php echo $ref_rel; ?>https://website.net">https://website.net</a>
+							</p>
+							<div class="ltn__blog-meta">
+								<ul>
+									<li class="ltn__blog-date">
+										<a href="#"><i class="far fa-calendar-alt"></i>June 22, 2020</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="popular-post-widget-item clearfix">
+						<div class="popular-post-widget-img">
+							<a href="#"><i class="fab fa-twitter"></i></a>
+						</div>
+						<div class="popular-post-widget-brief">
+							<p>Carsafe - #Gutenberg ready
+								@wordpress
+								Theme for Car Service, Auto Parts, Car Dealer available on
+								@website
+								<a href="<?php echo $ref_rel; ?>https://website.net">https://website.net</a>
+							</p>
+							<div class="ltn__blog-meta">
+								<ul>
+									<li class="ltn__blog-date">
+										<a href="#"><i class="far fa-calendar-alt"></i>June 22, 2020</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<!-- Instagram Widget -->
+		<div class="widget ltn__instagram-widget">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Instagram</h4>
+			<div class="ltn__instafeed ltn__instafeed-grid insta-grid-gutter"></div>
+		</div>
+		<!-- Tagcloud Widget -->
+		<div class="widget ltn__tagcloud-widget">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Etiquetas Populares</h4>
+			<ul>
+				<li><a href="#">Acción</a></li>
+				<li><a href="#">Aventura</a></li>
+				<li><a href="#">RPG</a></li>
+				<li><a href="#">Simulación</a></li>
+				<li><a href="#">Carreras de Autos</a></li>
+				<li><a href="#">Arcade e Indie</a></li>
+				<li><a href="#">Tecnologia</a></li>
+				<li><a href="#">Gadgets</a></li>
+				<li><a href="#">Gameplays de Juegos</a></li>
+				<li><a href="#">Minecraft 1.92</a></li>
+				<li><a href="#">Vegeta 777</a></li>
+				<li><a href="#">Dispositivos Móviles</a></li>
+				<li><a href="#">eSports</a></li>
+				<li><a href="#">Consolas</a></li>
+				<li><a href="#">Juegos para PC</a></li>
+				<li><a href="#">Gameplays de RPG</a></li>
+				<li><a href="#">MMORPG</a></li>
+				<li><a href="#">Puzzle</a></li>
+				<li><a href="#">Juegos Shooter</a></li>
+				<li><a href="#">Cooperativo</a></li>
+				<li><a href="#">Eventos de Anime</a></li>
+			</ul>
+		</div>
+		<!-- Banner Widget -->
+		<div class="widget ltn__instagram-widget">
+			<h4 class="ltn__widget-title ltn__widget-title-border">Próximamente...</h4>
+			<div class="ltn__instafeed ltn__instafeed-grid insta-grid-gutter"><a href="<?php echo $ref_rel; ?>shop.php"><img src="<?php echo $ref_rel; ?>img/banner/banner-4.jpg" alt="Banner Image"></a></div>
+		</div>
