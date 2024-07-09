7a23927074e585c65a3fb11469a155e67e2ac425
marcosd59 <damian.marcospool@gmail.com>
2024-07-09 08:15:30 -0500

    feat: Add video popup area to the webpage

---
+++ modules/video-area.php
@@ -0,0 +1,13 @@
+<div class="ltn__video-popup-area ltn__video-popup-margin">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="ltn__video-bg-img ltn__video-popup-height-600 bg-overlay-black-10-- bg-image" data-bg="<?php echo $ref_rel; ?>img/bg/15.png">
+					<a class="ltn__video-icon-2 ltn__video-icon-2-border" href="https://www.youtube.com/embed/I3rjToYvn84?autoplay=1&showinfo=0" data-rel="lightcase:myCollection">
+						<i class="fa fa-play"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
