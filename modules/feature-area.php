348bfea93afa3602213ea279b0083d16b856f6c9
marcosd59 <damian.marcospool@gmail.com>
2024-07-10 09:36:32 -0500

    feat: Add feature area to the webpage

---
+++ modules/feature-area.php
@@ -0,0 +1,64 @@
+<div class="ltn__feature-area before-bg-bottom-2 mb--30--- plr--5">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="ltn__feature-item-box-wrap ltn__border-between-column white-bg">
+					<div class="row">
+						<div class="col-xl-3 col-md-6 col-12">
+							<div class="ltn__feature-item ltn__feature-item-8">
+								<div class="ltn__feature-icon">
+									<img src="<?php echo $ref_rel; ?>img/icons/icon-img/11.png" alt="#" />
+								</div>
+								<div class="ltn__feature-info">
+									<h4>Colecciones Exclusivas</h4>
+									<p>
+										Descubre nuestra gama exclusiva de productos.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-3 col-md-6 col-12">
+							<div class="ltn__feature-item ltn__feature-item-8">
+								<div class="ltn__feature-icon">
+									<img src="<?php echo $ref_rel; ?>img/icons/icon-img/12.png" alt="#" />
+								</div>
+								<div class="ltn__feature-info">
+									<h4>Accesorios Premium</h4>
+									<p>
+										Descubre nuestra gama de accesorios de gaming.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-3 col-md-6 col-12">
+							<div class="ltn__feature-item ltn__feature-item-8">
+								<div class="ltn__feature-icon">
+									<img src="<?php echo $ref_rel; ?>img/icons/icon-img/13.png" alt="#" />
+								</div>
+								<div class="ltn__feature-info">
+									<h4>Gaming Auténtico</h4>
+									<p>
+										Nuestros productos son nuevos y genuinos.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-3 col-md-6 col-12">
+							<div class="ltn__feature-item ltn__feature-item-8">
+								<div class="ltn__feature-icon">
+									<img src="<?php echo $ref_rel; ?>img/icons/icon-img/14.png" alt="#" />
+								</div>
+								<div class="ltn__feature-info">
+									<h4>Envío Complementario</h4>
+									<p>
+										Disfruta de la entrega a domicilio en todo el país.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
