bfe97c0d9605693237acb3ca8cef6b1368763281
marcosd59 <damian.marcospool@gmail.com>
2024-07-10 09:36:23 -0500

    feat: Add countdown area to the webpage

---
+++ modules/countdown-area.php
@@ -0,0 +1,24 @@
+<div class="ltn__call-to-action-area ltn__call-to-action-4 section-bg-1 pt-110 pb-120">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-5">
+				<img src="<?php echo $ref_rel; ?>img/banner/11.png" alt="#" />
+			</div>
+			<div class="col-lg-7">
+				<div class="call-to-action-inner call-to-action-inner-4 text-color-white--- text-center---">
+					<div class="section-title-area ltn__section-title-2 text-center---">
+						<h6 class="ltn__secondary-color">Oferta del mes</h6>
+						<h1 class="section-title">
+							Oferta Exclusiva <br />
+							De Potencia Gaming
+						</h1>
+					</div>
+					<div class="ltn__countdown ltn__countdown-3 bg-white--" data-countdown="2024/11/13"></div>
+					<div class="btn-wrapper animated">
+						<a href="<?php echo $ref_rel; ?>shop.php" class="theme-btn-1 btn btn-effect-1 text-uppercase">Compra ahora</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
