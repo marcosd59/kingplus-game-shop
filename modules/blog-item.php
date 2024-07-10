5e0a8ccf6abcc3c492feb9f5cc7cde1b2896926d
marcosd59 <damian.marcospool@gmail.com>
2024-07-10 09:38:07 -0500

    feat: Add blog item sections to the webpage

---
+++ modules/blog-item.php
@@ -0,0 +1,52 @@
+<!-- Blog Item (Quote) -->
+<div class="ltn__blog-item ltn__blog-item-5 ltn__blog-item-quote bg-image bg-overlay-theme-90" data-bg="img/blog/3.jpg">
+	<div class="ltn__blog-brief">
+		<blockquote>
+			<a href="blog-details.php">Excepteur sint occaecat cupida
+				tat non proident, sunt in.</a>
+		</blockquote>
+		<div class="ltn__blog-meta mb-0">
+			<ul>
+				<li>
+					<a href="#"><i class="far fa-eye"></i>232 Views</a>
+				</li>
+				<li>
+					<a href="#"><i class="far fa-comments"></i>35 Comments</a>
+				</li>
+				<li class="ltn__blog-date">
+					<i class="far fa-calendar-alt"></i>June 22, 2020
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!-- Blog Item (Quote) End-->
+
+<!-- Blog Item (Background Image) -->
+<div class="ltn__blog-item ltn__blog-item-5 ltn__blog-item-bg-image bg-image bg-overlay-white-90" data-bg="img/blog/2.jpg">
+	<div class="ltn__blog-brief">
+		<div class="ltn__blog-meta">
+			<ul>
+				<li class="ltn__blog-category">
+					<a href="#">Business</a>
+				</li>
+			</ul>
+		</div>
+		<h3 class="ltn__blog-title"><a href="blog-details.php">Culpa qui officia deserunt mollit anim
+				id est laborum. Sed ut perspiciatis</a></h3>
+		<div class="ltn__blog-meta mb-0">
+			<ul>
+				<li>
+					<a href="#"><i class="far fa-eye"></i>232 Views</a>
+				</li>
+				<li>
+					<a href="#"><i class="far fa-comments"></i>35 Comments</a>
+				</li>
+				<li class="ltn__blog-date">
+					<i class="far fa-calendar-alt"></i>June 22, 2020
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!-- Blog Item (Background Image) End -->
