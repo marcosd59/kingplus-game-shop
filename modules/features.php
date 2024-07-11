39d958ce30782607154d24c2973f5bb3c63cfb10
marcosd59 <damian.marcospool@gmail.com>
2024-07-11 08:22:13 -0500

    feat: Add feature area to the webpage

---
+++ modules/features.php
@@ -0,0 +1,53 @@
+<div class="ltn__feature-area section-bg-1 pt-115 pb-90">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="section-title-area ltn__section-title-2 text-center">
+					<h6 class="section-subtitle ltn__secondary-color">// features //</h6>
+					<h1 class="section-title">Why Choose Us<span>.</span></h1>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-lg-4 col-sm-6 col-12">
+				<div class="ltn__feature-item ltn__feature-item-7">
+					<div class="ltn__feature-icon-title">
+						<div class="ltn__feature-icon">
+							<span><img src="img/icons/icon-img/21.png" alt="#"></span>
+						</div>
+						<h3><a href="service-details.php">All Kind Brand</a></h3>
+					</div>
+					<div class="ltn__feature-info">
+						<p>Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-sm-6 col-12">
+				<div class="ltn__feature-item ltn__feature-item-7">
+					<div class="ltn__feature-icon-title">
+						<div class="ltn__feature-icon">
+							<span><img src="img/icons/icon-img/22.png" alt="#"></span>
+						</div>
+						<h3><a href="service-details.php">Curated Products</a></h3>
+					</div>
+					<div class="ltn__feature-info">
+						<p>Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-sm-6 col-12">
+				<div class="ltn__feature-item ltn__feature-item-7">
+					<div class="ltn__feature-icon-title">
+						<div class="ltn__feature-icon">
+							<span><img src="img/icons/icon-img/23.png" alt="#"></span>
+						</div>
+						<h3><a href="service-details.php">Pesticide Free Goods</a></h3>
+					</div>
+					<div class="ltn__feature-info">
+						<p>Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
