8eb4113e278b2aae7868ded00b53354731bd2724
marcosd59 <damian.marcospool@gmail.com>
2024-07-11 08:21:36 -0500

    feat: Add mobile menu component to the webpage

---
+++ modules/mobile-menu.php
@@ -0,0 +1,76 @@
+<div id="ltn__utilize-mobile-menu" class="ltn__utilize ltn__utilize-mobile-menu">
+	<div class="ltn__utilize-menu-inner ltn__scrollbar">
+		<div class="ltn__utilize-menu-head">
+			<div class="site-logo">
+				<a href="<?php echo $ref_rel; ?>index.php"><img src="<?php echo $ref_rel; ?>img/logo-1.png" height="80%" alt="Logo" /></a>
+			</div>
+			<button class="ltn__utilize-close">×</button>
+		</div>
+		<div class="ltn__utilize-menu-search-form">
+			<form action="#">
+				<input type="text" placeholder="Buscar..." />
+				<button><i class="fas fa-search"></i></button>
+			</form>
+		</div>
+		<div class="ltn__utilize-menu">
+			<ul>
+				<li>
+					<a href="<?php echo $ref_rel; ?>index.php">Inicio</a>
+				</li>
+				<li>
+					<a href="<?php echo $ref_rel; ?>shop.php">Tienda</a>
+				</li>
+				<li>
+					<a href="<?php echo $ref_rel; ?>blog.php">Blog</a>
+				</li>
+				<li><a href="<?php echo $ref_rel; ?>contact.php">Contacto</a></li>
+			</ul>
+		</div>
+		<div class="ltn__utilize-buttons ltn__utilize-buttons-2">
+			<ul>
+				<li>
+					<a href="<?php echo $ref_rel; ?>account.php" title="My Account">
+						<span class="utilize-btn-icon">
+							<i class="far fa-user"></i>
+						</span>
+						Mi Cuenta
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo $ref_rel; ?>wishlist.php" title="Wishlist">
+						<span class="utilize-btn-icon">
+							<i class="far fa-heart"></i>
+							<sup>3</sup>
+						</span>
+						Lista de Deseos
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo $ref_rel; ?>cart.php" title="Shoping Cart">
+						<span class="utilize-btn-icon">
+							<i class="fas fa-shopping-cart"></i>
+							<sup>2</sup>
+						</span>
+						Carrito de Compra
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="ltn__social-media-2">
+			<ul>
+				<li>
+					<a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+				</li>
+				<li>
+					<a href="#" title="Twitter"><i class="fab fa-twitter"></i></a>
+				</li>
+				<li>
+					<a href="#" title="Linkedin"><i class="fab fa-linkedin"></i></a>
+				</li>
+				<li>
+					<a href="#" title="Instagram"><i class="fab fa-instagram"></i></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
