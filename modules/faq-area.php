190ed914d87d31499600bdfb3dbe8b2e773eee0f
marcosd59 <damian.marcospool@gmail.com>
2024-07-11 08:20:31 -0500

    feat: Add FAQ area to the webpage

---
+++ modules/faq-area.php
@@ -0,0 +1,88 @@
+<div class="ltn__faq-area pt-115 pb-120">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="section-title-area ltn__section-title-2 text-center">
+					<h1 class="section-title white-color---">Some Questions</h1>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="ltn__faq-inner ltn__faq-inner-2">
+					<div id="accordion_2">
+						<!-- card -->
+						<div class="card">
+							<h6 class="collapsed ltn__card-title" data-bs-toggle="collapse" data-bs-target="#faq-item-2-1" aria-expanded="false">
+								How to buy a product?
+							</h6>
+							<div id="faq-item-2-1" class="collapse" data-parent="#accordion_2">
+								<div class="card-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Proin libero nunc consequat interdum. Condimentum lacinia quis vel eros donec ac.</p>
+								</div>
+							</div>
+						</div>
+						<!-- card -->
+						<div class="card">
+							<h6 class="ltn__card-title" data-bs-toggle="collapse" data-bs-target="#faq-item-2-2" aria-expanded="true">
+								How can i make refund from your website?
+							</h6>
+							<div id="faq-item-2-2" class="collapse show" data-parent="#accordion_2">
+								<div class="card-body">
+									<div class="ltn__video-img alignleft">
+										<img src="img/bg/17.jpg" alt="video popup bg image">
+										<a class="ltn__video-icon-2 ltn__video-icon-2-small ltn__video-icon-2-border----" href="https://www.youtube.com/embed/LjCzPp-MK48?autoplay=1&showinfo=0" data-rel="lightcase:myCollection">
+											<i class="fa fa-play"></i>
+										</a>
+									</div>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque </p>
+								</div>
+							</div>
+						</div>
+						<!-- card -->
+						<div class="card">
+							<h6 class="collapsed ltn__card-title" data-bs-toggle="collapse" data-bs-target="#faq-item-2-3" aria-expanded="false">
+								I am a new user. How should I start?
+							</h6>
+							<div id="faq-item-2-3" class="collapse" data-parent="#accordion_2">
+								<div class="card-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Proin libero nunc consequat interdum. Condimentum lacinia quis vel eros donec ac.</p>
+								</div>
+							</div>
+						</div>
+						<!-- card -->
+						<div class="card">
+							<h6 class="collapsed ltn__card-title" data-bs-toggle="collapse" data-bs-target="#faq-item-2-4" aria-expanded="false">
+								Returns and refunds
+							</h6>
+							<div id="faq-item-2-4" class="collapse" data-parent="#accordion_2">
+								<div class="card-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Proin libero nunc consequat interdum. Condimentum lacinia quis vel eros donec ac.</p>
+								</div>
+							</div>
+						</div>
+						<!-- card -->
+						<div class="card">
+							<h6 class="collapsed ltn__card-title" data-bs-toggle="collapse" data-bs-target="#faq-item-2-5" aria-expanded="false">
+								Are my details secured?
+							</h6>
+							<div id="faq-item-2-5" class="collapse" data-parent="#accordion_2">
+								<div class="card-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Proin libero nunc consequat interdum. Condimentum lacinia quis vel eros donec ac. Mauris sit amet massa vitae tortor. Quisque id diam vel quam elementum pulvinar. Gravida in fermentum et sollicitudin ac orci phasellus. Facilisis gravida neque convallis a cras semper. Non arcu risus quis varius quam quisque id.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<aside class="sidebar-area ltn__right-sidebar mt-60">
+					<!-- Banner Widget -->
+					<div class="widget ltn__banner-widget">
+						<a href="shop.php"><img src="img/bg/12.png" alt="Banner Image"></a>
+					</div>
+				</aside>
+			</div>
+		</div>
+	</div>
+</div>
