147e87a0373f8c7c4983bb93021e7e2abda3cc8a
marcosd59 <damian.marcospool@gmail.com>
2024-07-11 08:21:56 -0500

    feat: Add team member section to the webpage

---
+++ modules/team.php
@@ -0,0 +1,85 @@
+<div class="ltn__team-area pt-115 pb-90">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="section-title-area ltn__section-title-2 text-center">
+					<h1 class="section-title white-color---">Team Member</h1>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-xl-3 col-lg-4 col-sm-6">
+				<div class="ltn__team-item">
+					<div class="team-img">
+						<img src="img/team/1.jpg" alt="Image">
+					</div>
+					<div class="team-info">
+						<h6 class="ltn__secondary-color">// founder //</h6>
+						<h4><a href="team-details.php">Rosalina D. William</a></h4>
+						<div class="ltn__social-media">
+							<ul>
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-lg-4 col-sm-6">
+				<div class="ltn__team-item">
+					<div class="team-img">
+						<img src="img/team/2.jpg" alt="Image">
+					</div>
+					<div class="team-info">
+						<h6 class="ltn__secondary-color">// founder //</h6>
+						<h4><a href="team-details.php">Rosalina D. William</a></h4>
+						<div class="ltn__social-media">
+							<ul>
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-lg-4 col-sm-6">
+				<div class="ltn__team-item">
+					<div class="team-img">
+						<img src="img/team/3.jpg" alt="Image">
+					</div>
+					<div class="team-info">
+						<h6 class="ltn__secondary-color">// founder //</h6>
+						<h4><a href="team-details.php">Rosalina D. William</a></h4>
+						<div class="ltn__social-media">
+							<ul>
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-lg-4 col-sm-6">
+				<div class="ltn__team-item">
+					<div class="team-img">
+						<img src="img/team/4.jpg" alt="Image">
+					</div>
+					<div class="team-info">
+						<h6 class="ltn__secondary-color">// founder //</h6>
+						<h4><a href="team-details.php">Rosalina D. William</a></h4>
+						<div class="ltn__social-media">
+							<ul>
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
