ab4e462ad7234adce3fae211a048e9fdbf33f1b5
marcosd59 <damian.marcospool@gmail.com>
2024-07-11 08:20:49 -0500

    feat: Add head section to the webpage

---
+++ modules/head.php
@@ -0,0 +1,19 @@
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<title>KingPlus | Game Shop</title>
+	<meta name="robots" content="noindex, follow" />
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+	<!-- Place favicon.png in the root directory -->
+	<link rel="shortcut icon" href="<?php echo $ref_rel; ?>img/favicon.png" type="image/x-icon" />
+	<!-- Font Icons css -->
+	<link rel="stylesheet" href="<?php echo $ref_rel; ?>css/font-icons.css" />
+	<!-- plugins css -->
+	<link rel="stylesheet" href="<?php echo $ref_rel; ?>css/plugins.css" />
+	<!-- Main Stylesheet -->
+	<link rel="stylesheet" href="<?php echo $ref_rel; ?>css/style.css" />
+	<!-- Responsive css -->
+	<link rel="stylesheet" href="<?php echo $ref_rel; ?>css/responsive.css" />
+</head>
