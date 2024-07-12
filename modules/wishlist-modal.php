ca514894496c0975cd3573b99870777b9ede0465
marcosd59 <damian.marcospool@gmail.com>
2024-07-12 07:26:51 -0500

    feat: Add wishlist modal component

---
+++ modules/wishlist-modal.php
@@ -0,0 +1,48 @@
+<div class="ltn__modal-area ltn__add-to-cart-modal-area">
+	<div class="modal fade" id="liton_wishlist_modal" tabindex="-1">
+		<div class="modal-dialog modal-md" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="ltn__quick-view-modal-inner">
+						<div class="modal-product-item">
+							<div class="row">
+								<div class="col-12">
+									<div class="modal-product-img">
+										<img src="<?php echo $ref_rel; ?>img/banner/banner-10.png" width="80%" alt="#" style="margin-top:1.5rem; margin-left:1rem;" />
+									</div>
+									<div class="modal-product-info">
+										<h5>
+											<a href="product-details.php">Producto</a>
+										</h5>
+										<p class="added-cart">
+											<i class="fa fa-check-circle"></i> Añadido exitosamente a tu lista de deseos
+										</p>
+										<div class="btn-wrapper">
+											<a href="#" class="theme-btn-1 btn btn-effect-1" data-bs-dismiss="modal">Cerrar</a>
+										</div>
+									</div>
+									<!-- additional-info -->
+									<div class="additional-info d-none">
+										<p>
+											We want to give you <b>10% discount</b> for your
+											first order, <br />
+											Use discount code at checkout
+										</p>
+										<div class="payment-method">
+											<img src="<?php echo $ref_rel; ?>img/icons/payment.png" alt="#" />
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
