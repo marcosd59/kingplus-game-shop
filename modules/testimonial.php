feb1e15e3b9ef11892fcdad5d592b861a4b2c535
marcosd59 <damian.marcospool@gmail.com>
2024-07-12 07:26:59 -0500

    feat: Add testimonial section to the webpage

---
+++ modules/testimonial.php
@@ -0,0 +1,76 @@
+<div class="ltn__testimonial-area section-bg-1 pt-290 pb-70">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="section-title-area ltn__section-title-2 text-center">
+					<h6 class="section-subtitle ltn__secondary-color">
+						// Testimonios
+					</h6>
+					<h1 class="section-title">Comentarios de los clientes<span>.</span></h1>
+				</div>
+			</div>
+		</div>
+		<div class="row ltn__testimonial-slider-3-active slick-arrow-1 slick-arrow-1-inner">
+			<div class="col-lg-12">
+				<div class="ltn__testimonial-item ltn__testimonial-item-4">
+					<div class="ltn__testimoni-info">
+						<p>"¡KingPlus siempre ha entregado productos de excelente calidad y lo último en tecnología! Su servicio al cliente es insuperable. ¡Es mi fuente principal para todas mis necesidades de videojuegos!"</p>
+						<h4>Alex Hart</h4>
+						<h6>Entusiasta de los Videojuegos</h6>
+					</div>
+					<div class="ltn__testimoni-bg-icon">
+						<i class="far fa-comments"></i>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12">
+				<div class="ltn__testimonial-item ltn__testimonial-item-4">
+					<div class="ltn__testimoni-info">
+						<p>"Desde el proceso de compra hasta la entrega, todo es fluido. Me encantan las ofertas exclusivas en tecnología de alta gama. ¡Definitivamente recomendado para los amantes de la tecnología!"</p>
+						<h4>Sarah Johnson</h4>
+						<h6>Aficionada a la Tecnología</h6>
+					</div>
+					<div class="ltn__testimoni-bg-icon">
+						<i class="far fa-comments"></i>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12">
+				<div class="ltn__testimonial-item ltn__testimonial-item-4">
+					<div class="ltn__testimoni-info">
+						<p>"Compré mi primer portátil para videojuegos en KingPlus y estoy encantado con el rendimiento. El soporte al cliente me ayudó a elegir el modelo perfecto para mis necesidades."</p>
+						<h4>Michael Reeves</h4>
+						<h6>Nuevo Jugador</h6>
+					</div>
+					<div class="ltn__testimoni-bg-icon">
+						<i class="far fa-comments"></i>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12">
+				<div class="ltn__testimonial-item ltn__testimonial-item-4">
+					<div class="ltn__testimoni-info">
+						<p>"¡Servicio absolutamente fantástico! KingPlus ofrece excelentes precios y sus productos siempre son lo que espero. También tienen envíos súper rápidos y confiables."</p>
+						<h4>Emma Brown</h4>
+						<h6>Cliente Satisfecho</h6>
+					</div>
+					<div class="ltn__testimoni-bg-icon">
+						<i class="far fa-comments"></i>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12">
+				<div class="ltn__testimonial-item ltn__testimonial-item-4">
+					<div class="ltn__testimoni-info">
+						<p>"La variedad de productos en KingPlus es increíble. Siempre puedo encontrar algo nuevo y emocionante a un gran precio. Su atención al cliente también es muy útil y amigable."</p>
+						<h4>Lisa Turner</h4>
+						<h6>Compradora Frecuente</h6>
+					</div>
+					<div class="ltn__testimoni-bg-icon">
+						<i class="far fa-comments"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
