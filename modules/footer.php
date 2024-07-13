570bdda37db1fd905a2b3b3293ad8abe6e3490ab
marcosd59 <damian.marcospool@gmail.com>
2024-07-13 06:33:32 -0500

    feat: Add footer section to the webpage

---
+++ modules/footer.php
@@ -0,0 +1,176 @@
+<footer class="ltn__footer-area">
+	<div class="footer-top-area section-bg-1 plr--5">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-3 col-md-6 col-sm-6 col-12">
+					<div class="footer-widget footer-about-widget">
+						<div class="footer-logo">
+							<div class="site-logo">
+								<img src="<?php echo $ref_rel; ?>img/logo-1.png" width="50%" alt="Logo" />
+							</div>
+						</div>
+						<p>
+							En KingPlus, nos apasiona ofrecerte lo mejor en entretenimiento de videojuegos.
+						</p>
+						<div class="footer-address">
+							<ul>
+								<li>
+									<div class="footer-address-icon">
+										<i class="icon-placeholder"></i>
+									</div>
+									<div class="footer-address-info">
+										<p>Cancún, Quintana Roo.</p>
+									</div>
+								</li>
+								<li>
+									<div class="footer-address-icon">
+										<i class="icon-call"></i>
+									</div>
+									<div class="footer-address-info">
+										<p><a href="tel:+0123-456789">+52 998-116-0272</a></p>
+									</div>
+								</li>
+								<li>
+									<div class="footer-address-icon">
+										<i class="icon-mail"></i>
+									</div>
+									<div class="footer-address-info">
+										<p>
+											<a href="mailto:example@example.com">KingPlus@gmail.com</a>
+										</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="ltn__social-media mt-20">
+							<ul>
+								<li>
+									<a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+								</li>
+								<li>
+									<a href="#" title="Twitter"><i class="fab fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="#" title="Linkedin"><i class="fab fa-linkedin"></i></a>
+								</li>
+								<li>
+									<a href="#" title="Youtube"><i class="fab fa-youtube"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-2 col-md-6 col-sm-6 col-12">
+					<div class="footer-widget footer-menu-widget clearfix">
+						<h4 class="footer-title">Compañía</h4>
+						<div class="footer-menu">
+							<ul>
+								<li><a href="<?php echo $ref_rel; ?>about.php">Acerca de</a></li>
+								<li><a href="<?php echo $ref_rel; ?>blog.php">Blog</a></li>
+								<li><a href="<?php echo $ref_rel; ?>shop.php">Todos los Productos</a></li>
+								<li><a href="<?php echo $ref_rel; ?>locations.php">Mapa de Ubicaciones</a></li>
+								<li><a href="<?php echo $ref_rel; ?>faq.php">Preguntas Frecuentes</a></li>
+								<li><a href="<?php echo $ref_rel; ?>contact.php">Contáctanos</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-2 col-md-6 col-sm-6 col-12">
+					<div class="footer-widget footer-menu-widget clearfix">
+						<h4 class="footer-title">Servicios</h4>
+						<div class="footer-menu">
+							<ul>
+								<li><a href="<?php echo $ref_rel; ?>order-tracking.php">Rastreo de Pedido</a></li>
+								<li><a href="<?php echo $ref_rel; ?>wishlist.php">Lista de Deseos</a></li>
+								<li><a href="<?php echo $ref_rel; ?>login.php">Iniciar Sesión</a></li>
+								<li><a href="<?php echo $ref_rel; ?>account.php">Mi Cuenta</a></li>
+								<li><a href="<?php echo $ref_rel; ?>about.php">Términos y Condiciones</a></li>
+								<li><a href="<?php echo $ref_rel; ?>about.php">Ofertas Promocionales</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-2 col-md-6 col-sm-6 col-12">
+					<div class="footer-widget footer-menu-widget clearfix">
+						<h4 class="footer-title">Atención al Cliente</h4>
+						<div class="footer-menu">
+							<ul>
+								<li><a href="<?php echo $ref_rel; ?>login.php">Iniciar Sesión</a></li>
+								<li><a href="<?php echo $ref_rel; ?>account.php">Mi Cuenta</a></li>
+								<li><a href="<?php echo $ref_rel; ?>wishlist.php">Lista de Deseos</a></li>
+								<li><a href="<?php echo $ref_rel; ?>order-tracking.php">Rastreo de Pedido</a></li>
+								<li><a href="<?php echo $ref_rel; ?>faq.php">Preguntas Frecuentes</a></li>
+								<li><a href="<?php echo $ref_rel; ?>contact.php">Contáctanos</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-md-6 col-sm-12 col-12">
+					<div class="footer-widget footer-newsletter-widget">
+						<h4 class="footer-title">Boletín Informativo</h4>
+						<p>
+							Suscríbete a nuestro boletín semanal y recibe actualizaciones por
+							correo electrónico.
+						</p>
+						<div class="footer-newsletter">
+							<div id="mc_embed_signup">
+								<form action="https://gmail.us5.list-manage.com/subscribe/post?u=dde0a42ff09e8d43cad40dc82&amp;id=72d274d15d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+									<div id="mc_embed_signup_scroll">
+										<div class="mc-field-group">
+											<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Correo Electrónico*" />
+										</div>
+										<div id="mce-responses" class="clear">
+											<div class="response" id="mce-error-response" style="display: none"></div>
+											<div class="response" id="mce-success-response" style="display: none"></div>
+										</div>
+										<!-- personas reales no deberían llenar esto y esperar cosas buenas - no elimines esto o arriesgas inscripciones de bots -->
+										<div style="position: absolute; left: -5000px" aria-hidden="true">
+											<input type="text" name="b_dde0a42ff09e8d43cad40dc82_72d274d15d" tabindex="-1" value="" />
+										</div>
+										<div class="clear">
+											<div class="btn-wrapper">
+												<button class="theme-btn-1 btn" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe">
+													<i class="fas fa-location-arrow"></i>
+												</button>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<h5 class="mt-30">Aceptamos</h5>
+						<img src="<?php echo $ref_rel; ?>img/icons/payment-4.png" alt="Imagen de Pago" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="ltn__copyright-area ltn__copyright-2 section-bg-2 ltn__border-top-2--- plr--5">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6 col-12">
+					<div class="ltn__copyright-design clearfix">
+						<p>
+							Todos los Derechos Reservados @ KingPlus
+							<span class="current-year"></span>
+						</p>
+					</div>
+				</div>
+				<div class="col-md-6 col-12 align-self-center">
+					<style>
+						.ul li a:hover {
+							color: #f4a632;
+						}
+					</style>
+					<div class="ltn__copyright-menu text-end ul li a">
+						<ul>
+							<li><a href="#">Términos y Condiciones</a></li>
+							<li><a href="#">Reclamo</a></li>
+							<li><a href="#">Privacidad y Política</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
