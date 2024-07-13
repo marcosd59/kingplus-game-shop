29ab31c0de53952811889b83bd66a3d366a5d5f3
marcosd59 <damian.marcospool@gmail.com>
2024-07-13 06:33:40 -0500

    feat: Add slider area to the webpage

---
+++ modules/slider-area.php
@@ -0,0 +1,77 @@
+<div class="ltn__slider-area ltn__slider-3 section-bg-1">
+	<div class="ltn__slide-one-active slick-slide-arrow-1 slick-slide-dots-1">
+		<!-- ltn__slide-item -->
+		<div class="ltn__slide-item ltn__slide-item-2 ltn__slide-item-3">
+			<div class="ltn__slide-item-inner">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 align-self-center">
+							<div class="slide-item-info">
+								<div class="slide-item-info-inner ltn__slide-animation">
+									<div class="slide-video mb-50 d-none">
+										<a class="ltn__video-icon-2 ltn__video-icon-2-border" href="https://www.youtube.com/embed/CIv63jbc26s?autoplay=1&showinfo=0" data-rel="lightcase:myCollection">
+											<i class="fa fa-play"></i>
+										</a>
+									</div>
+									<h6 class="slide-sub-title animated">
+										<img src="<?php echo $ref_rel; ?>img/icons/icon-img/1.png" alt="#" /> 100% Experiencia de Juego
+									</h6>
+									<h1 class="slide-title animated">
+										Tecnología y <br />
+										Videojuegos
+									</h1>
+									<div class="slide-brief animated d-none">
+										<p>
+											Somos una tienda de videojuegos y tecnología, ofrecemos productos de calidad y garantía.
+										</p>
+									</div>
+									<div class="btn-wrapper animated">
+										<a href="shop.php" class="theme-btn-1 btn btn-effect-1 text-uppercase">Explorar Productos</a>
+									</div>
+
+								</div>
+							</div>
+							<div class="slide-item-img">
+								<img src="<?php echo $ref_rel; ?>img/slider/23.png" alt="slider23" onerror="this.style.display='none'" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- ltn__slide-item -->
+		<div class="ltn__slide-item ltn__slide-item-2 ltn__slide-item-3">
+			<div class="ltn__slide-item-inner text-right text-end">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 align-self-center">
+							<div class="slide-item-info">
+								<div class="slide-item-info-inner ltn__slide-animation">
+									<h6 class="slide-sub-title animated">
+										<img src="<?php echo $ref_rel; ?>img/icons/icon-img/1.png" alt="icono" /> Productos Originales
+									</h6>
+									<h1 class="slide-title animated">
+										Descubre la Magia <br />
+										con Nintendo Switch
+									</h1>
+									<div class="slide-brief animated">
+										<p>
+											Experimenta la consola de juegos para el hogar definitiva que va donde tú vas.
+										</p>
+									</div>
+									<div class="btn-wrapper animated">
+										<a href="shop.php" class="theme-btn-1 btn btn-effect-1 text-uppercase">Explorar Productos</a>
+										<a href="about.php" class="btn btn-transparent btn-effect-3 text-uppercase">LEER MAS</a>
+									</div>
+								</div>
+							</div>
+							<div class="slide-item-img slide-img-left">
+								<img src="img/slider/21.png" width="60%" alt="slider21" onerror="this.style.display='none'" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
