d579c5b96fb4e3174d96f3f9c6eb42a02407611c
marcosd59 <damian.marcospool@gmail.com>
2024-07-14 09:20:20 -0500

    feat: Add newsletter area to the webpage

---
+++ modules/newsletter-area.php
@@ -0,0 +1,18 @@
+<div class="ltn__newsletter-area section-bg-1 bg-overlay-black-90 pt-110 pb-90 bg-image" data-bg="img/bg/2.jpg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 offset-lg-2">
+				<div class="ltn__newsletter-inner text-center">
+					<h1 class="white-color">Boletin Informativo</h1>
+					<p class="white-color">Suscríbete a nuestro boletín semanal y recibe actualizaciones por correo electrónico. <br> acceso exclusivo a precios de lanzamiento anticipado de productos y más.</p>
+					<form action="#" class="ltn__form-box">
+						<input type="email" name="email" placeholder="Correo electrónico*">
+						<div class="btn-wrapper">
+							<button class="theme-btn-1 btn btn-effect-1 text-uppercase" type="submit">Suscribirse</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
